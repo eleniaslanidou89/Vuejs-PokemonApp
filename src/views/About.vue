e1e1484244239808c53b8c9fe19d5bfcb128bf3e
Eleni <helena.aslanidou@gmail.com>
2021-10-03 21:24:40 +0200

    added API and parameters

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{$route.params.slug}}</h2>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,33 @@
     <h3>
       Hello World
     </h3>
+    {{ pokemons }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { reactive, toRefs } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-   
-  }
-}
+  name: "Home",
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+    });
+
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+    return { ...toRefs(state) };
+  },
+};
 </script>
